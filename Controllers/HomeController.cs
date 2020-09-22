c0b4981ec4f11cd45197a8d059e173fa8c12ba07
Manuj Sharma <technomanuj@hotmail.com>
2020-09-22 20:18:55 +0530

    added solution file

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,22 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+
 using TestMVC.Models;
 
 namespace TestMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+            public HomeController()
+            {
+                
+            }
 
         public IActionResult Index()
         {
